ff241c65a36f16d206793297752f699d248ff2c4
MUNDUS3684 <guy110849@gmail.com>
2026-07-25 11:54:23 +0700

    Backup commit

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -19,12 +20,23 @@ public class Ball : MonoBehaviour , IPointerClickHandler
 
     [SerializeField]
     private BallColor color;
-    
+
+    [SerializeField]
+    private MeshRenderer rd;
+
+    [SerializeField]
+    private GameObject[] BallPosition;
+
     public void  OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(point);
         GameManager.instance.Playerscore += point;
         Destroy(gameObject);
+    }
+
+    void Awake()
+    {
+
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -38,4 +50,53 @@ public class Ball : MonoBehaviour , IPointerClickHandler
     {
         
     }
+
+    public void SetColorAndPoint(BallColor col)
+    {
+        color = col;
+
+        switch (col)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = Color.brown;
+                break;
+
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = Color.pink;
+                break;
+
+            case BallColor.Black:
+                point = 7;
+                rd.material.color = Color.black;
+                break;
+        }
+    }
+
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 
+
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private int playerscore;
     public int Playerscore { get { return playerscore; } set { playerscore = value; } }
+
+    [SerializeField]
+    private GameObject[] ballPositions;
+
+    [SerializeField]
+    private GameObject ballPrefab;
 
     public static GameManager instance;
 
@@ -15,12 +22,23 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SetBall(BallColor.Red, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetBall(BallColor col, int i)
+    {
+        GameObject obj = Instantiate(ballPrefab,
+            ballPositions[i].transform.position,
+            Quaternion.identity);
+
+        Ball b = obj.GetComponent<Ball>();
+        b.SetColorAndPoint(col);
+
     }
 }
