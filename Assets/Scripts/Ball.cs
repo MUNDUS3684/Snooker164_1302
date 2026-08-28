53b294bc0517067e5084aefeafdfd7e30a2c42c9
MUNDUS3684 <guy110849@gmail.com>
2026-08-29 02:50:00 +0700

    Finished

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,9 +1,8 @@
-using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public enum BallColor
-{ 
+{
     White,
     Red,
     Yellow,
@@ -14,45 +13,91 @@ public enum BallColor
     Black
 }
 
-
 public class Ball : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] private BallColor color;
-    [SerializeField] private int point = 0;
+    [SerializeField]
+    private int point;
 
+    public int Point
+    {
+        get { return point; }
+        set { point = value; }
+    }
 
-    [SerializeField] private Material[] materials = new Material[8];
-    private MeshRenderer meshRenderer;
+    [SerializeField]
+    private MeshRenderer rd;
+
+    [SerializeField]
+    private BallColor color;
+
+    public BallColor Color
+    {
+        get { return color; }
+    }
+
+    private void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log(point);
-        GameManager.instance.PlayerScore += point;
+        if (GameManager.instance == null)
+        {
+            return;
+        }
+
+        GameManager.instance.AddScore(point);
+        GameManager.instance.RemoveBall(color);
+
         Destroy(gameObject);
     }
 
-    void OnValidate()
+    public void SetColorAndPoint(BallColor col)
     {
-        meshRenderer = GetComponent<MeshRenderer>();
-        point = (int)color;
-        meshRenderer.material = materials[point];
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+        color = col;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void Color(BallColor ball)
-    {
-        color = ball;
-        point = (int)color;
-        meshRenderer = GetComponent<MeshRenderer>();
-        meshRenderer.material = materials[point];
+        switch (col)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = UnityEngine.Color.white;
+                break;
+
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = UnityEngine.Color.red;
+                break;
+
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = UnityEngine.Color.yellow;
+                break;
+
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = UnityEngine.Color.green;
+                break;
+
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = new UnityEngine.Color(0.59f, 0.29f, 0.1f);
+                break;
+
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = UnityEngine.Color.blue;
+                break;
+
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = new UnityEngine.Color(1f, 0.4f, 0.7f);
+                break;
+
+            case BallColor.Black:
+                point = 7;
+                rd.material.color = UnityEngine.Color.black;
+                break;
+        }
     }
 }
